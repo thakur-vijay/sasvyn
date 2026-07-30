27bb84f0fcf04b3b40a8bab1943e91c89d1865e7
ThakurVijay210901 <105584645+ThakurVijay2191@users.noreply.github.com>
2026-07-31 01:20:14 +0530

    Initial Commit

---
+++ Sasvyn/SasvynApp.swift
@@ -0,0 +1,17 @@
+//
+//  SasvynApp.swift
+//  Sasvyn
+//
+//  Created by Vijay Thakur on 31/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SasvynApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SasvynTests/SasvynTests.swift
@@ -0,0 +1,18 @@
+//
+//  SasvynTests.swift
+//  SasvynTests
+//
+//  Created by Vijay Thakur on 31/07/26.
+//
+
+import Testing
+
+struct SasvynTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
